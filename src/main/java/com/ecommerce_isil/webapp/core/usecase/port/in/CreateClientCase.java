e0181a3e4cd4dfcebc2b7eea225cbb3b20364b67
Gabriel <gabrielcontacto08@gmail.com>
2026-05-17 13:42:43 -0500

    feature/agregar getclient

--- src/main/java/com/ecommerce_isil/webapp/core/usecase/port/in/CreateClientCase.java
+++ src/main/java/com/ecommerce_isil/webapp/core/usecase/port/in/CreateClientCase.java
@@ -3,6 +3,9 @@ package com.ecommerce_isil.webapp.core.usecase.port.in;
 import com.ecommerce_isil.webapp.core.usecase.dto.request.CreateClientRequest;
 import com.ecommerce_isil.webapp.core.usecase.dto.response.ClientResponse;
 
+import java.util.UUID;
+
 public interface CreateClientCase {
     ClientResponse createClient(CreateClientRequest request);
+    ClientResponse getClient(UUID id);
 }
